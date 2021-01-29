90337860d2686ec4c7211eb75a44695cef70f5b8
alldakr <alldakr@gmail.com>
2021-01-29 13:30:32 +0900

    Paper-Kit Pro React 템플릿 추가(Bootstrap)

---
+++ styling-react/src/App.js
@@ -0,0 +1,15 @@
+import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponents";
+
+function App() {
+  return (
+    <div>
+      <SassComponent />
+      <CSSModule />
+      <StyledComponent />
+    </div>
+  );
+}
+
+export default App;
